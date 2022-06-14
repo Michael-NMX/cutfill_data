e47761452628e9a084f373354f30e6104614aaf6
Michael-NMX <michael.castillo@noltemx.com>
2022-06-14 00:44:14 -0500

    Feat: Read files from dirpath and extract data
    Two functions are need:
    - readFiles() iterates over files found in user dirpath
    - extractDataFromFile() reads HTML to get all tables

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import pathlib
+import pandas as pd
 
 
 def getDirpath():
@@ -6,6 +7,15 @@ def getDirpath():
     dirpath = pathlib.Path(dirpathFromUser)
     return dirpath
 
+def readFiles(dirpath):
+    filepaths = [filepath for filepath in dirpath.iterdir() if filepath.is_file()]
+    dataTables = [extractDataFromFile(filepath) for filepath in filepaths]
+    return dataTables
+
+def extractDataFromFile(filepath):
+    return pd.read_html(filepath, flavor='bs4')
+
 
 if __name__ == '__main__':
-    getDirpath()
+    dirpath = getDirpath()
+    data = readFiles(dirpath)
